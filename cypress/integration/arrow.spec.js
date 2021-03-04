66109de11ef8b66164eeae561e3749ab89b77912
diegolsouzaa <diegolsouzaa@gmail.com>
2021-03-04 08:54:07 -0300

    arrow functions

---
+++ cypress/integration/arrow.spec.js
@@ -0,0 +1,41 @@
+it('nada agora', function(){})
+
+//function soma (a,b){
+//    return a+b;
+//}
+
+//const soma = function(a,b){
+//    return a+b;
+//}
+
+//const soma = (a,b)=>{
+//    return a+b
+//}
+
+//const soma = (a,b)=> a+b
+
+// undefined
+//const soma = (a,b)=>{
+//    a+b
+//}
+
+//const soma = (a)=> a + a
+
+//const soma = a => a + a
+
+//const soma = () => 5+5
+
+//console.log(soma(3,3))
+
+it('a function test...', function(){
+
+console.log('Function', this)
+})
+
+//nao pega o context
+//it('an Arrow test...', () => {
+//console.log('Arrow', this)
+//})
+
+
+
