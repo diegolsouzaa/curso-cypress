d5c359a95deb7cfcddef13ed0caeaa124dc06052
diegolsouzaa <diegolsouzaa@gmail.com>
2021-03-08 17:33:22 -0300

    promises

---
+++ cypress/integration/promises.spec.js
@@ -0,0 +1,23 @@
+it('sem testes', () => { })
+
+const getSomething = ()=>{
+return new Promise((resolve, reject) => {
+setTimeout(()=> {
+    resolve(13);
+}, 1000)
+
+})
+
+
+}
+
+const system = () => {
+    console.log('init');
+    const prom = getSomething();
+    prom.then(some=> {
+    console.log(`Something is ${some}`)
+    })
+    console.log('end')
+}
+
+system();
